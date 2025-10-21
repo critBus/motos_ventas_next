fda87a36a8ca88217010ef1aa7831622c2f6ade4
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-10-21 19:53:05 -0400

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/`;
+
+export const GMAIL_USER = process.env.GMAIL_USER;
+export const GMAIL_PASS = process.env.GMAIL_PASS;
+export const SENT_EMAIL = process.env.SENT_EMAIL == "true";
+export const DOMAIN_URL = process.env.DOMAIN_URL;
